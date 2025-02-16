b316df496aedcee5fc36cab84327d71ef3bfaa3e
Divyansh Gupta <divyansh170704@gmail.com>
2025-02-16 12:41:05 +0530

    first

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='header-contents'>
+            <h2>Order your favourite food here</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio velit id aspernatur provident assumenda enim maiores unde fugit, ex doloremque nam quas obcaecati consequatur ad quae numquam quia. Itaque repudiandae iste totam.</p>
+            <button>View Menu</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
